cdf23efd52946a44f0e7eb02188d020872702fdf
Fabián <GneccoFR@gmail.com>
2020-09-04 18:31:18 -0300

    Base character and enemy classes
    Game general purpose scripts for Characters, and a inherited class for Enemies.
    Still have a lot of work to do.

---
+++ Character.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    //Essential components
+	Rigidbody2D myRigidBody;
+    AudioSource audioSource;
+    Animator animator;
+
+    //Movement properties
+    Vector2 lookDirection = new Vector2(1, 0);
+    float horizontal;
+    float vertical;
+    public float speed = 3f;
+
+    //Health properties
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+    bool alive;
+
+    //Invincibility properties
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
+
+    //Builds it's main components
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+        alive = true;
+    }
+
+    //Once per frame
+    void Update()
+    {
+        //if the character is not alive halts the function
+        if (!alive) return;
+
+        //Executes Animation movement
+        MoveAnimation();
+
+        //Checks it's invincibility status
+        InvincibleCheck();
+    }
+
+    //Once per physical tick
+    void FixedUpdate()
+    {
+        if (!alive) return;
+        //Execute it's physical movement
+        PhysicalMove();
+    }
+
+    //Reduces it's invincible timer if active, or shuts it off if reached the fixed time
+    public void InvincibleCheck()
+    {
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+    }
+
+    //Sets the animator float values with the asked direction
+    public void MoveAnimation()
+    {
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        CheckDirection(move);
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+    }
+
+    //checks the direction the character is facing
+    public void CheckDirection(Vector2 direction)
+    {
+        if (!Mathf.Approximately(direction.x, 0.0f) || !Mathf.Approximately(direction.y, 0.0f))
+        {
+            lookDirection.Set(direction.x, direction.y);
+            lookDirection.Normalize();
+        }
+    }
+
+    //takes the given values for speed and direction and assigns it as movement to it's body
+    public void PhysicalMove()
+    {
+
+        Vector2 position = rigidbody2D.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        rigidbody2D.MovePosition(position);
+    }
+
+    public abstract void OnCollisionEvent(Collision2D collision);
+}
---
+++ Enemy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class Enemy : Character
+{
+    //list of it's inherent components
+	protected QuestManager questManager;
+    
+    //this is specific of the implementation
+    public AudioClip bonkClip;
+    public AudioClip fixClip;
+    public ParticleSystem smokeEffect;
+    public ParticleSystem hitEffect;
+    
+    //Damage value for contact attacks
+    public int enemyDamage;
+    
+    //general AI properties to implement in further variety of enemies
+    public float changeTime = 3.0f;
+    protected float timer;
+    protected bool bonk;
+
+
+    protected void Start()
+    {
+        //Builds its essential components from the Character Class
+        base.Start();
+
+        //builds its own components
+        questManager = QuestManager.GetInstance();
+        timer = changeTime;
+    }
+
+    protected void ProjectileCollision(Collision2D collision)
+    {
+        Projectile projectileCollided = collision.gameObject.GetComponent<Projectile>();
+        if (projectileCollided != null)
+        {
+            audioSource.PlayOneShot(bonkClip);
+            Fix();
+        }
+    }
+
+    protected void PlayerCollision(Collision2D collision)
+    {
+        RubyController playerCollided = collision.gameObject.GetComponent<RubyController>();
+        if (playerCollided != null)
+        {
+            audioSource.PlayOneShot(bonkClip);
+            playerCollided.ChangeHealth(-enemyDamage);
+        }
+    }
+
+    protected void WallCollision(Collision2D collision)
+    {
+        Projectile projectileCollided = collision.gameObject.GetComponent<Projectile>();
+        RubyController playerCollided = collision.gameObject.GetComponent<RubyController>();
+        if (playerCollided == null && projectileCollided == null)
+            bonk = true;
+    }
+
+
+}
